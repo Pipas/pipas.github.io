820ce2b5aa2b74b519fa79bc62485a4dcc07b970
Pipas <paulo.renato.a.correia@gmail.com>
2019-08-28 18:21:38 +0100

    Refactor project structure, add scroll animations, footer section and new fonts

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+new Glide('.glide', {
+  type: 'carousel',
+  startAt: 0,
+  perView: 6,
+  gap: 48,
+  autoplay: 3000
+}).mount()
+
+let controller = new ScrollMagic.Controller();
+
+document.querySelectorAll('h2.title').forEach(element => {
+  new ScrollMagic.Scene({
+    triggerElement: element,
+    triggerHook: 0.4
+  })
+    .on('start', function() {
+      element.classList.add('underline')
+    })
+    .addTo(controller)
+})
+
+const footer = document.querySelector('#footer')
+new ScrollMagic.Scene({
+  triggerElement: footer,
+  triggerHook: 1,
+  offset: footer.clientHeight * 0.75
+})
+  .on('start', function() {
+    footer.classList.add('animate')
+  })
+  .addTo(controller)
+
